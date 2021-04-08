d2ca2f0b9ce8948c92731f2c6915f7edff184d3c
Caitlin Raby <caitlinraby@gmail.com>
2021-04-08 10:48:36 -0500

    commit tree a7409f456cabc1574ddf6718bc1907362bed8a3f

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-// Code your solution in this file!
+let distanceOne;
+function distanceFromHqInBlocks(distanceOne) {
+    if (distanceOne >= 42) {
+        return distanceOne - 42;
+    } else {
+            return 42 - distanceOne;
+        }
+}
+
+function distanceFromHqInFeet(distanceOne) {
+    return distanceFromHqInBlocks(distanceOne) * 264;
+}
+
+function distanceTravelledInFeet(num1, num2) {
+    if (num1 > num2) {
+        return (num1 - num2) * 264;
+    } else {
+        return (num2 - num1) * 264;
+    }
+}
+
+function calculatesFarePrice (num1, num2) { 
+    if (distanceTravelledInFeet(num1, num2) <= 400) {
+            return 0;
+    } else if (distanceTravelledInFeet(num1, num2) > 400 && distanceTravelledInFeet(num1, num2) < 2000) {
+        return (distanceTravelledInFeet(num1, num2) - 400) * 0.02;
+    } else if (distanceTravelledInFeet(num1, num2) >= 2000 && distanceTravelledInFeet(num1, num2) <= 2500) {
+        return 25;
+    } else if (distanceTravelledInFeet(num1, num2) > 2500) {
+        return "cannot travel that far";
+    }
+}
